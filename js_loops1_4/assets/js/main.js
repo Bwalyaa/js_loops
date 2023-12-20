19879cf16e77b588f5c658a89510a95c43d1e515
Bwalyaa <bwalya@hotmail.de>
2023-12-20 14:13:28 +0100

    erste aufgabenbox finish

---
+++ js_loops1_4/assets/js/main.js
@@ -0,0 +1,5 @@
+let names = ["Georg", "Anass", "Elaine", "Hakan", "Eric", "Kim", "Sergio"];
+
+for (let name of names){
+    console.log(name);
+}
---
+++ js_loops1_6/assets/js/main.js
@@ -0,0 +1,8 @@
+let array = [];
+
+for (let i = 1; i <= 100; i++) {
+    let imageName = "image_" + i + ".jpg";
+    array.push(imageName);
+}
+
+console.log(array);
